d85a9290e632c59c4a306127864dc4ae61a0da46
ZMarkgo <351479188.qq.com>
2023-02-26 15:25:06 +0800

    perf(sh):shell wait pid

--- test/runpy.sh
+++ test/runpy.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
 # Must be whole path
 usr="/home/vm2"
-pyScriptPath="/home/vm2/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql/ads_fuzzer.py"
+pyScriptPath="${usr}/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql/ads_fuzzer.py"
 runpyOutPath="${usr}/ADSTestProject/ADSTDP-Deploy/log/runpy.out"
 testCaseId=$1
-mkdir /home/vm2/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
-python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
+mkdir ${usr}/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
+nohup python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
+pid=$!
+wait $pid
+if [ $? -eq 0 ]; then
+    echo "PYTHON_SCRIPT_SUCCESS"
+else
+    echo "PYTHON_SCRIPT_FAILURE"
+fi
