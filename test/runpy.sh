1debc721e0c85402834d8b0e4e16c0351727caae
ZMark <351479188@qq.com>
2023-02-26 15:41:14 +0800

    docs(.sh):为runpy.sh添加注释

--- test/runpy.sh
+++ test/runpy.sh
@@ -6,8 +6,11 @@ runpyOutPath="${usr}/ADSTestProject/ADSTDP-Deploy/log/runpy.out"
 testCaseId=$1
 mkdir ${usr}/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
 nohup python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
+# $! 获取到最后一个在后台执行的进程ID
+# $pid 保存该进程ID，并使用 wait 命令等待该进程执行完毕
 pid=$!
 wait $pid
+# 判断python脚本是否执行成功
 if [ $? -eq 0 ]; then
     echo "PYTHON_SCRIPT_SUCCESS"
 else
