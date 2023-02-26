7406b56e7947f61db9a59390e349b2729fc97891
ZMark <351479188@qq.com>
2023-02-26 14:34:00 +0800

    Update runpy.sh

--- test/runpy.sh
+++ test/runpy.sh
@@ -3,9 +3,6 @@
 usr="/home/vm2"
 pyScriptPath="/home/vm2/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql/ads_fuzzer.py"
 runpyOutPath="${usr}/ADSTestProject/ADSTDP-Deploy/log/runpy.out"
-# echo $pyScriptPath
-# echo $runpyOutPath
-testCaseId=50
+testCaseId=$1
 mkdir /home/vm2/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
 python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
-# <test_id>是指当前正在准备运行的实例的testid
