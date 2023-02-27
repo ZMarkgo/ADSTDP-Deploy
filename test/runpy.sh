ef08838c9b7a7180e0e595037371776f89e3b5a3
ZMarkgo <351479188.qq.com>
2023-02-27 17:03:26 +0800

    fix

--- test/runpy.sh
+++ test/runpy.sh
@@ -4,6 +4,7 @@ usr="/home/vm2"
 pyScriptPath="${usr}/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql/ads_fuzzer.py"
 runpyOutPath="${usr}/ADSTestProject/ADSTDP-Deploy/log/runpy.out"
 testCaseId=$1
+cd ${usr}/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql
 mkdir ${usr}/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
 nohup python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
 # $! 获取到最后一个在后台执行的进程ID
