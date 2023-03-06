b18c3ec83d917a2af891731f97ac2ab7407329a7
ZMark <351479188@qq.com>
2023-03-07 01:08:46 +0800

    refactor(.sh):修改启动测试的shell脚本以支持停止测试

--- test/runpy.sh
+++ test/runpy.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+# 定义处理 SIGTERM 信号的函数
+function on_sigterm() {
+    kill $pythonPID
+    exit 0
+}
+
+# 注册 SIGTERM 信号处理函数
+trap on_sigterm SIGTERM
+
 # Must be whole path
 usr="/home/vm2"
 pyScriptPath="${usr}/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql/ads_fuzzer.py"
@@ -8,12 +17,7 @@ cd ${usr}/ADSTestProject/ADSTest_sql/ADS-FUZZER_sql
 mkdir ${usr}/ADSTestProject/ADSTest_sql/test_file/${testCaseId}
 nohup python3 ${pyScriptPath} --test_id ${testCaseId} > ${runpyOutPath} 2>&1 &
 # $! 获取到最后一个在后台执行的进程ID
-# $pid 保存该进程ID，并使用 wait 命令等待该进程执行完毕
-pid=$!
-wait $pid
-# 判断python脚本是否执行成功
-if [ $? -eq 0 ]; then
-    echo "PYTHON_SCRIPT_SUCCESS"
-else
-    echo "PYTHON_SCRIPT_FAILURE"
-fi
+# $pid 保存该进程ID
+# 使用 wait 命令等待该进程执行完毕
+pythonPID=$!
+wait $pythonPID
